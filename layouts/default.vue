6d7416f571a29dabd1a63cd087ae6d713206a4be
FiRE <fire@saar-lan.de>
2026-03-14 23:35:21 +0100

    Refactor layout structure and styling in default.vue
    Updated main section padding and added a wrapper div for content.

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,13 @@
         <NuxtLink to="/" class="mb-6 block text-xl font-bold text-primary">ezSWM</NuxtLink>
         <AppSidebar />
       </aside>
+
       <div class="col-span-12 flex min-h-screen flex-col md:col-span-9 lg:col-span-10">
         <AppHeader />
-        <main class="flex-1 p-4">
-          <slot />
+        <main class="flex-1 px-4 py-6">
+          <div class="mx-auto w-full max-w-7xl">
+            <slot />
+          </div>
         </main>
         <AppFooter />
       </div>
